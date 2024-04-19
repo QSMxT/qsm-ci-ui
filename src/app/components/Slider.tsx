6f61d138d6354ce4943c2a39e25d46c145397256
iishiishii <love_sweet_98@yahoo.com.vn>
2024-04-19 14:03:26 +1000

    image range set to -0.2,0.2. symmetric slider range. add favicon.

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { Slider } from "antd";
 
-export default function RangeSlider(props: any) {
-  let sliderRange: any = {
+interface SliderProps {
+  nv: any;
+  pick: boolean;
+}
+
+interface SliderRange {
+  defaultRange: number[];
+  increment: number;
+}
+
+export default function RangeSlider({ nv, pick }: SliderProps) {
+  let sliderRange: SliderRange = {
     defaultRange: [-0.2, 0.2],
     increment: 0.01,
   };
@@ -35,23 +45,33 @@ export default function RangeSlider(props: any) {
   }
 
   React.useEffect(() => {
-    Promise.race([
-      waitUntilNvIsUp(props.nv),
-      waitForDuration(LOADING_TIMEOUT),
-    ]).then((nv: any) => {
-      try {
-        setRange([nv.volumes[0].cal_min, nv.volumes[0].cal_max]);
-      } catch (error) {
-        console.error(error);
+    Promise.race([waitUntilNvIsUp(nv), waitForDuration(LOADING_TIMEOUT)]).then(
+      (nv: any) => {
+        try {
+          nv.volumes[0].cal_min = range[0];
+          nv.volumes[0].cal_max = range[1];
+          nv.updateGLVolume();
+        } catch (error) {
+          console.error(error);
+        }
       }
-    });
-  }, [props.pick, props.nv]);
+    );
+  }, [pick, nv]);
 
-  function onRangeChange(newValue: number | number[]) {
-    setRange(newValue);
-    props.nv.volumes[0].cal_min = range[0];
-    props.nv.volumes[0].cal_max = range[1];
-    props.nv.updateGLVolume();
+  function onRangeChange(newValue: number[]) {
+    let symmetricRange;
+    if (newValue[0] !== range[0] && newValue[1] === range[1]) {
+      symmetricRange = [newValue[0], -newValue[0]];
+    } else if (newValue[1] !== range[1] && newValue[0] === range[0]) {
+      symmetricRange = [-newValue[1], newValue[1]];
+    } else {
+      symmetricRange = newValue;
+    }
+    setRange(symmetricRange);
+
+    nv.volumes[0].cal_min = range[0];
+    nv.volumes[0].cal_max = range[1];
+    nv.updateGLVolume();
   }
 
   return (
@@ -60,7 +80,7 @@ export default function RangeSlider(props: any) {
       value={range}
       max={defaultRange[1]}
       min={defaultRange[0]}
-      onChange={onRangeChange}
+      onAfterChange={onRangeChange}
       range
       disabled={false}
     />
